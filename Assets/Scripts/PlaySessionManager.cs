0ab1906e6b3ef158857b76e3df232ef497436d29
Conor Costain <conorcostain@gmail.com>
2017-11-28 21:13:02 +0000

    Started work on a Play Session Manager
    Trying to setup my scene management

---
+++ Assets/Scripts/PlaySessionManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaySessionManager : MonoBehaviour {
+
+	public static PlaySessionManager ins;
+
+	private void Awake()
+	{
+		if(ins == null)
+		{
+			ins = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else if(ins != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+	}
+
+	public void RoundOver(int playerNumber)
+	{
+		Debug.Log("Player" + playerNumber + "has Lost");
+		Invoke("Restart", 1f);
+	}
+
+	private void Restart()
+	{
+		
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void LoadScene(string sceneName)
+	{
+		
+		SceneManager.LoadScene(sceneName);
+	}
+
+	
+}
