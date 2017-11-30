689ff4bcfe865d619604e8b8d4b4562e2365844f
Conor Costain <conorcostain@gmail.com>
2017-11-30 20:19:12 +0000

    Networking groundworks
    Started to Play around with networking, with not much luck

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	[SerializeField]
+	Behaviour[] ComponentsToBeDisabled;
+	// Use this for initialization
+	void Start () {
+		if (!isLocalPlayer)
+		{
+			foreach(Behaviour component in ComponentsToBeDisabled)
+			{
+				component.enabled = false;
+			}
+		}
+
+		
+	}
+	
+	
+}
